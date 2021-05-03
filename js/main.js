05c96977bcf78d9215781ae9ab23e59bf180adcd
Petar Veličković <petrovy.velickovic@gmail.com>
2021-05-03 10:20:24 +0100

    JS cut 2

--- js/main.js
+++ js/main.js
@@ -7,9 +7,6 @@ sitemap:
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-    $('.panel-cover').css('max-width', currentWidth)
-    $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
